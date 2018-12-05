ee009a8aac3221c8d22f42ae44608275e3b05abb
Izzywizz <isfandyar.ali@me.com>
2018-12-05 16:11:37 +0000

    added a script to be able to switch weapons.

--- Assets/Scripts/GunEquipper.cs
+++ Assets/Scripts/GunEquipper.cs
@@ -4,13 +4,58 @@ using UnityEngine;
 
 public class GunEquipper : MonoBehaviour {
 
-	// Use this for initialization
+    public static string activeWeaponType;
+
+    public GameObject pistol;
+    public GameObject assaultRifle;
+    public GameObject shotgun;
+
+    GameObject activeGun;
+
 	void Start () {
-		
+        // Starting gun is initilized to be the pistol
+        activeWeaponType = Constants.Pistol;
+        activeGun = pistol;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        //Checking the input for each frame lke this is known as Polling
+        if (Input.GetKeyDown("1"))
+        {
+            loadWeapon(pistol);
+            activeWeaponType = Constants.Pistol;
+        } 
+        else if (Input.GetKeyDown("2"))
+        {
+            loadWeapon(assaultRifle);
+            activeWeaponType = Constants.AssaultRifle;
+        }
+        else if (Input.GetKeyDown("3"))
+        {
+            loadWeapon(shotgun);
+            activeWeaponType = Constants.Shotgun;
+        }
+    }
+
+    /// <summary>
+    /// Gets the active weapon. Recall the activeGun is private thus no one can modify but this method helps see what it is.
+    /// </summary>
+    /// <returns>The active weapon.</returns>
+    public GameObject GetActiveWeapon()
+    {
+        return activeGun;
+    }
+
+    ///Change and load weapon
+    private void loadWeapon(GameObject weapon)
+    {
+        pistol.SetActive(false);
+        assaultRifle.SetActive(false);
+        shotgun.SetActive(false);
+
+        weapon.SetActive(true);
+        activeGun = weapon;
+    }
 }
