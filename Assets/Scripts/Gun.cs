db84cf95cd61ab3cab0b7134c918fbca7222209c
Izzywizz <isfandyar.ali@me.com>
2018-12-31 16:54:52 +0000

    sound added for weapons

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,6 +6,10 @@ public class Gun : MonoBehaviour {
 
     public float fireRate; //rate of fire
     protected float lastFireTime; //time since last blast
+    public Ammo ammo; //used for tracking gun ammunition
+    public AudioClip liveFire;
+    public AudioClip dryFire;
+
 
 	// Use this for initialization
 	void Start () {
@@ -19,7 +23,17 @@ public class Gun : MonoBehaviour {
 
     protected void Fire() // Remember each weapon fire will have its own slight variable on these methods
     {
-        //
+        //Checks if the player has bullet, fire away with the sound, if not play the empty clip. It should consume ammo when fired
+        if (ammo.HasAmmo(tag))
+        {
+            GetComponent<AudioSource>().PlayOneShot(liveFire);
+            ammo.ConsumeAmmo(tag);
+        }
+        else
+        {
+            GetComponent<AudioSource>().PlayOneShot(dryFire);
+        }
+        //grabs the animator controller for weapon in use and plays its animatior
         GetComponentInChildren<Animator>().Play("Fire");
     }
 }
